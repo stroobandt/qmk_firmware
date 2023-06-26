ae070cfe776447ace093c2354b138fb89ba722d2
Serge Y. Stroobandt <1105595+stroobandt@users.noreply.github.com>
2023-06-27 01:21:12 +0200

    jj40 morse & mf68

--- keyboards/40percentclub/mf68/keymaps/stroobandt/keymap.c
+++ keyboards/40percentclub/mf68/keymaps/stroobandt/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 #define _QWERTY 0
-#define _FN1 1
+#define _FN 1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_68_ansi(
@@ -11,11 +11,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT,          KC_UP,
     KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
   ),
-  [_FN1] = LAYOUT_68_ansi(
+  [_FN] = LAYOUT_68_ansi(
     KC_PWR,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,          KC_MSEL, KC_HOME,
     KC_SLEP, KC_CALC, KC_WHOM, KC_MAIL, KC_WBAK, _______, _______, KC_UNDO, _______, _______, KC_PSCR, KC_BRID, KC_BRIU, KC_WSCH,          KC_MUTE, KC_END,
     KC_CAPS, KC_AGIN, KC_SLCT, KC_MYCM, KC_WFWD, _______, _______, KC_EJCT, _______, _______, KC_MSEL, KC_APP,           _______,
-    _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_MPRV, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_FIND,                   KC_MPLY,          KC_VOLU,
-    KC_WAKE, KC_APP,  _______,                            KC_MPLY,                            _______, _______, _______,          KC_MRWD, KC_VOLD, KC_MFFD
+    _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_MPRV, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_FIND,                   KC_MPLY,          KC_MS_U,
+    KC_WAKE, KC_APP,  _______,                            KC_MPLY,                            _______, _______, _______,          KC_MS_L, KC_MS_D, KC_MS_R
   )
 };
---
+++ keyboards/kprepublic/jj40/keymaps/morse/keymap.c
@@ -0,0 +1,57 @@
+/* Copyright 2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+#define _QWERTY 0
+#define _FN 1
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * |   1  |   2  |   3  |   4  |   5  | Esc  | Bksp |   6  |   7  |   8  |   9  |   0  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |   Q  |   W  |   E  |   R  |   T  |  F1  |  F2  |   Y  |   U  |   I  |   O  |   P  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |   A  |   S  |   D  |   F  |   G  | Func |   =  |   H  |   J  |   K  |   L  |   /  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |   Z  |   X  |   C  |   V  |   B  |    Enter    |   N  |   M  |   ,  |   .  | Ctrl |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_QWERTY] = LAYOUT_planck_mit(
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC,  KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F1,   KC_F2,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    MO(1),   KC_EQL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SLSH,
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_ENT,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RCTL
+),
+
+/* Function
+ * ,-----------------------------------------------------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |      | Del  |  F6  |  F7  |  F8  |  F9  | F10  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      | RGB  |      |      |      |      |      | Step |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |   +  |      |      |      | LED  |   ?  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      | Reset|             |      |      | Left | Right|      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_FN] = LAYOUT_planck_mit(
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, _______,  KC_DEL,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
+  _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, _______, BL_STEP, _______,
+  _______, _______, _______, RGB_TOG, _______, _______, KC_PLUS, _______, _______, _______, BL_TOGG, KC_QUES,
+  _______, _______, _______, _______, QK_BOOT,      _______,     _______, _______, KC_LEFT, KC_RGHT, _______
+)
+};
