597e1f8b7dad758197e3ea82f5d9e45d1dfb5efa
Serge Y. Stroobandt <1105595+stroobandt@users.noreply.github.com>
2023-07-02 13:14:29 +0200

    right arrow key; space

--- keyboards/kprepublic/jj40/keymaps/morse/keymap.c
+++ keyboards/kprepublic/jj40/keymaps/morse/keymap.c
@@ -27,14 +27,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |   A  |   S  |   D  |   F  |   G  | Func |   +  |   H  |   J  |   K  |   L  |   /  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |    Enter    |   N  |   M  | Left | Right| Ctrl |
+ * |   Z  |   X  |   C  |   V  |   B  |    Enter    |   N  |   M  | Left | Ctrl | Right|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_mit(
      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_ESC, KC_BSPC,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_F1,   KC_F6,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   MO(1), KC_PLUS,    KC_H,    KC_J,    KC_K,    KC_L, KC_SLSH,
-     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_ENT,        KC_N,    KC_M, KC_LEFT, KC_RGHT, KC_RCTL
+     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_ENT,        KC_N,    KC_M, KC_LEFT, KC_RCTL, KC_RGHT
 ),
 
 /* Function
@@ -45,13 +45,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |   =  |      |      |      | Light|   ?  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |Colour|Colour|      |             |      |      |   ,  |   .  |      |
+ * |      |      |Colour|Colour|      |    Space    |      |      |   ,  |      |   .  |
  * `-----------------------------------------------------------------------------------'
  */
 [_FN] = LAYOUT_planck_mit(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, _______,  KC_DEL,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, RGB_VAD, _______,
   _______, _______, _______, _______, _______, _______,  KC_EQL, _______, _______, _______, RGB_TOG, KC_QUES,
-  _______, _______, RGB_HUI, RGB_HUD, _______,      _______,     _______, _______, KC_COMM,  KC_DOT, _______
+  _______, _______, RGB_HUI, RGB_HUD, _______,       KC_SPC,     _______, _______, KC_COMM, _______,  KC_DOT
 )
 };
